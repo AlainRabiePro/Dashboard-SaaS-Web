6795c474b224cf938bd69f9d347bdabebf72de82
Alain <alain.rabie.pro@gmail.com>
2026-03-02 08:25:48 +0000

    Voila

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC3bnWt0iM2BtiCLDdU8ee80e9ZkX3t6FE",
+  authDomain: "interface-graphique-saas.firebaseapp.com",
+  projectId: "interface-graphique-saas",
+  storageBucket: "interface-graphique-saas.appspot.com",
+  messagingSenderId: "929209208166",
+  appId: "1:929209208166:web:4fde650fd1a9a4c0c60ca7"
 };
 
 export const isFirebaseConfigured = firebaseConfig.apiKey && firebaseConfig.apiKey !== 'YOUR_API_KEY';
